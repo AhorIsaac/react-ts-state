6e41669a90fa763746776d0b7fbec66c16531f08
AhorIsaac <ahorisaac.ai@gmail.com>
2023-03-19 14:10:54 +0100

    Add the Album

--- src/components/NewStateComp.tsx
+++ src/components/NewStateComp.tsx
@@ -34,6 +34,123 @@ class NewStateComp extends Component<IProps, IState> {
 
     return (
       <div className="App container text-center py-3">
+        {/* album */}
+        <div className="album py-5 bg-light">
+          <div className="container">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div className="col">
+                <div className="card shadow-sm">
+                  <img
+                    src="assets/images/71.jpg"
+                    className="bd-placeholder-img card-img-top"
+                    width="100%"
+                    height="225"
+                    alt="img"
+                  />
+
+                  <div className="card-body">
+                    <p className="card-text">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="btn-group">
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-primary"
+                        >
+                          View
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-primary"
+                        >
+                          Edit
+                        </button>
+                      </div>
+                      <small className="text-muted">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="card shadow-sm">
+                <img
+                    src="assets/images/72.jpg"
+                    className="bd-placeholder-img card-img-top"
+                    width="100%"
+                    height="225"
+                    alt="img"
+                  />
+
+                  <div className="card-body">
+                    <p className="card-text">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="btn-group">
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-primary"
+                        >
+                          View
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-primary"
+                        >
+                          Edit
+                        </button>
+                      </div>
+                      <small className="text-muted">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="card shadow-sm">
+                <img
+                    src="assets/images/73.jpg"
+                    className="bd-placeholder-img card-img-top"
+                    width="100%"
+                    height="225"
+                    alt="img"
+                  />
+
+                  <div className="card-body">
+                    <p className="card-text">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="btn-group">
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-primary"
+                        >
+                          View
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-primary"
+                        >
+                          Edit
+                        </button>
+                      </div>
+                      <small className="text-muted">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* end of album */}
+
         <h3 className="text-dark h3"> {message} </h3>
         <button className="btn btn-primary btn-md" onClick={this.buttonChange}>
           {" "}
